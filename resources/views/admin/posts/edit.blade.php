4559c9ed45196eb2dd1c96dbf96e84d9663c25e2
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-07-12 17:32:33 +0200

    esercizio completato

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -7,6 +7,17 @@
         <h3>Stai modificando: "{{$post->title}}"</h3>
         <p style="font-size: 18px">Modifica i dati e fai click su <span style="color: blue; text-decoration: underline">invia</span> per aggiornare i dati del post presente nel database.</p>
 
+        @if ($errors->any())
+        <div class="w-50 alert alert-danger">
+                <ul>
+                    {{-- $errors->all() reistituisce l'array ceon tutti gli errori --}}
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+        </div>
+        @endif
+
 
         <div class="w-50">
             <form action="{{ route('admin.posts.update', $post)}}" method="POST">
@@ -17,9 +28,15 @@
                     <label for="name" class="form-label sc-label">Titolo Post</label>
                     {{-- il name dell'input deve corrispondere al nome della colonna della tabella di riferimento --}}
                     <input type="text" id="name" name="title"
-                    value="{{$post->title }}"
-                    class="form-control"
+                    value="{{old('title', $post->title) }}"
+                    class="form-control @error('title') is-invalid @enderror"
                     placeholder="Titolo Post">
+
+                    @error('title')
+
+                        <p class="error-msg text-danger"> {{$message}} </p>
+
+                    @enderror
                 </div>
 
                 <div class="mb-3">
@@ -46,8 +63,40 @@
                 <div class="mb-3">
                     <label for="content" class="form-label">Content</label>
                     <textarea
-                    class="form-control"
-                    name="content" id="content" cols="30" rows="10">{{ $post->content }}</textarea>
+                    class="form-control @error('content') is-invalid @enderror"
+                    name="content" id="content" cols="30" rows="10">{{old('content', $post->content) }}</textarea>
+
+                    @error('content')
+
+                        <p class="error-msg text-danger"> {{$message}} </p>
+
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+
+                    @foreach ($tags as $tag)
+
+                        <input type="checkbox"
+                        name="tags[]"
+                        id="tag{{$loop->iteration}}"
+
+                        @if(!$errors->any() && $post->tags->contains($tag->id))
+
+                            checked
+
+                        @elseif ($errors->any() && in_array($tag->id, old('tags', []) ))
+
+                            checked
+
+                        @endif
+
+                        value="{{ $tag->id }}">
+
+                        <label class="mr-3" for="tag{{$loop->iteration}}">{{$tag->name}}</label>
+
+                    @endforeach
+
                 </div>
 
                 <button type="submit" class="btn btn-primary">Invia</button>
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,6 +5,12 @@
     <div class="container">
         <h1 class="mt-3 mb-4">Tutti i post esistenti</h1>
 
+        @if(session('prodotto_cancellato'))
+            <div class="alert alert-danger" role="alert">
+                {{ session('prodotto_cancellato') }}
+            </div>
+        @endif
+
         <table class="table">
             <thead>
               <tr>
@@ -26,7 +32,8 @@
 
                         <td>{{ $post->category ? $post->category->name : '-' }}</td>
 
-                        <td>@forelse ($post->tags as $tag)
+                        <td>
+                            @forelse ($post->tags as $tag)
 
                             <span class="badge text-bg-secondary">{{$tag->name}}</span>
 
@@ -42,7 +49,8 @@
                             <a class="btn btn-primary" href="{{route('admin.posts.edit', $post)}}">Edit</a>
 
                             <form class="d-inline"
-                             action=""
+                             action="{{route('admin.posts.destroy', $post)}}"
+                             onsubmit="return confirm('Confirm the action? Oance deleted it can\'t be restored')"
                              method="POST">
                              @csrf
                              @method('DELETE')
